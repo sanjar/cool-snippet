820076f6075ee9f5cd9b696e4d9675a007ace988
Mohammad Sadique Sanjar <sanjar.sadique@gmail.com>
2018-11-20 17:31:21 +0100

    Update randomMeshGenerator.js

--- randomMeshGenerator.js
+++ randomMeshGenerator.js
@@ -1,1 +1,9 @@
-
+for (var line=1; line<45; line++) {
+  for(var i=1;i<23;i++) {
+  var x = (Math.floor((Math.random()*2)%2)) ;
+  console.log(x);
+    var s = x ? "╱" : "╲";
+    document.write(s);
+  }
+  document.writeln("<br>");
+}
